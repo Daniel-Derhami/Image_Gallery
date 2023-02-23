a9cd6abd5caee407e51e9978250aba05eb8ae257
Daniel Derhami <daniel.derhami@gmail.com>
2023-02-23 22:01:13 +0100

    Commit fix JS

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const imagesList = [
   {
-    thumbUrl: "images/1-thumb.jpg",
-    imageUrl: "images/1.jpg",
+    thumbUrl: "images/001-thmb.jpg",
+    imageUrl: "images/001.jpg",
     title: "The First Title",
   },
 
   {
-    thumbUrl: "images/2-thumb.jpg",
-    imageUrl: "images/2.jpg",
+    thumbUrl: "images/002-thmb.jpg",
+    imageUrl: "images/002.jpg",
     title: "The Second Title",
+  },
+
+      {
+    thumbUrl: "images/003-thmb.jpg",
+    imageUrl: "images/003.jpg",
+    title: "The Third Title",
+  },
+
+  {
+    thumbUrl: "images/004-thmb.jpg",
+    imageUrl: "images/004.jpg",
+    title: "The Fourth Title",
   },
