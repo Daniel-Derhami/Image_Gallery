f510fa0d624cfc3106c94929f64528c986eaaee3
Daniel Derhami <daniel.derhami@gmail.com>
2023-02-23 22:00:07 +0100

    Commit JS

---
+++ main.js
@@ -0,0 +1,12 @@
+const imagesList = [
+  {
+    thumbUrl: "images/1-thumb.jpg",
+    imageUrl: "images/1.jpg",
+    title: "The First Title",
+  },
+
+  {
+    thumbUrl: "images/2-thumb.jpg",
+    imageUrl: "images/2.jpg",
+    title: "The Second Title",
+  },
